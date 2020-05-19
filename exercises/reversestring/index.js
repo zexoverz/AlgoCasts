06fc06e16e3803194095cb0ad219869fef593784
zexoverz <aryuical@gmail.com>
2020-05-19 17:40:56 +0700

    Finish ReverseString

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,34 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+//Solution 1
+
+// function reverse(str) {
+
+//     return str.split('').reverse().join('');
+
+// }
+
+//Solution 2
+
+function reverse(str) {
+
+    let result = ''
+
+    for (let char of str){
+        result  = char + result
+    }
+
+    return result
+}
+
+//Solution 3
+
+
+// function reverse(str) {
+//     return str.split('').reduce((reversed, char) => {
+//         return char + reversed
+//     })
+// }
 
 module.exports = reverse;
