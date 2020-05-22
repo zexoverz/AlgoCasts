e022e5b23de0ba14a60e6e7559cb7ef17137a392
zexoverz <aryuical@gmail.com>
2020-05-22 16:04:33 +0700

    Finish MaxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,18 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let map = {}
+    let maxChar = str[0]
+
+    for (let char of str){
+        map[char] = map[char] + 1 || 1
+        if(map[char] > map[maxChar]){
+            maxChar = char
+        }
+    }
+
+    return maxChar;
+}
 
 module.exports = maxChar;
