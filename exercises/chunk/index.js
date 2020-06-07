25866bebb465ccc37f5f38627a8794bd2623bdaa
zexoverz <aryuical@gmail.com>
2020-06-07 22:39:48 +0700

    Finish Chunk Problem

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,24 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    let result = [];
+    let tempArray = [];
+    
+    if(array.length < size){
+        result.push(array);
+        return result
+    }
+
+    for (let i = 0; i < array.length; i++){
+        tempArray.push(array[i]);
+        if(tempArray.length == size || i == array.length - 1){
+            result.push(tempArray);
+            tempArray = []
+        }
+    }
+
+    return result;
+}
 
 module.exports = chunk;
