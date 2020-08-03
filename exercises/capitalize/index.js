947e6bda9adf14980f8681db21fa05417b607106
zexoverz <aryuical@gmail.com>
2020-08-03 11:32:43 +0700

    Finish Capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,38 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+
+// Great Soultion
+
+function capitalize(str) {
+    let result = str[0].toUpperCase();
+
+
+    for (let i = 1; i < str.length; i++){
+        if (str[i - 1] == " "){
+            result += str[i].toUpperCase();
+        }else {
+            result += str[i]
+        }
+    }
+
+    return result;
+}
+
+
+// My Brute Force
+
+// function capitalize(str) {
+//     let splitWord = str.split(" ");
+//     let temp = []
+
+//     for (let word of splitWord){
+//         let strSplit = word.split("");
+//         strSplit[0] = word[0].toUpperCase();
+//         temp.push(strSplit.join(""));
+//     }
+
+//     return temp.join(" ");
+// }
 
 module.exports = capitalize;
