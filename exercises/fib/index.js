f79768ac4031e48c4a874ad3e22a271da91167b1
zexoverz <aryuical@gmail.com>
2020-09-19 14:15:19 +0700

    add memoization to fib function

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,10 @@
 // Example:
 //   fib(4) === 3
 
+
+// Iterative Solution O(n)
+
+
 // function fib(n) {
 //     let series = [0, 1];  // 0 1 
 //     let pointer = 0;
@@ -22,13 +26,34 @@
 
 // my recursive solution;
 
-function fib(n){
+// Do Memoization for Increase Algorithm Performance
+
+// slowFib >>> Memoization >>> fastFib
+
+function memoize(fn){
+    let cache = {}; // store fibonacci same number iteration
+
+    return function(...args){
+        if(cache[args]){
+            return cache[args];
+        }
+
+        const result = fn.apply(this, args);
+        cache[args] = result;
+
+        return result;
+    }
+}
+
+function slowFib(n){ // this is slow fib
     if(n < 2){
         return n
     }
 
     return fib(n - 1) + fib(n - 2);
 }
+
+const fib = memoize(slowFib);
 
 
 module.exports = fib;
